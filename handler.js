c6ad4b84ed70ce5e41f864c542a33e593d455d81
Jeff Wagner <jeff@moonrise.works>
2021-02-20 17:12:07 -0600

    move schema and resolvers to separate files

--- handler.js
+++ handler.js
@@ -1,22 +1,9 @@
-const { ApolloServer, gql } = require('apollo-server-lambda');
-// const { schema } = require('./schema');
-// const { resolvers } = require('./resolvers');
-
-const typeDefs = gql`
-  type Query {
-    hello: String
-  }
-`;
-
-// Provide resolver functions for your schema fields
-const resolvers = {
-  Query: {
-    hello: () => 'Hello world!',
-  },
-};
+const { ApolloServer } = require('apollo-server-lambda');
+const { schema } = require('./schema');
+const { resolvers } = require('./resolvers');
 
 const server = new ApolloServer({
-  typeDefs,
+  typeDefs: schema,
   resolvers,
 });
 
