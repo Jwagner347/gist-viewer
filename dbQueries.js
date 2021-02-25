bd3e92fbc764b88ba9cc3d4e8716cbee782cbeb1
Jeff Wagner <jeff@moonrise.works>
2021-02-25 17:45:31 -0600

    move createFavorite function to dbQueries

--- dbQueries.js
+++ dbQueries.js
@@ -3,8 +3,6 @@ const createDbClient = require('./utils/createDbClient');
 
 // todo: change favorites response in schema to return everything
 
-// todo: extract db stuff into a separate file
-
 module.exports = {
 		getAllFavorites: async () => {
 			const client = createDbClient(dbConfig);
@@ -15,5 +13,23 @@ module.exports = {
 			// todo: if insert fails, throw error 
 			return {favorites: rows};
 		},
+		createFavorite: async ({gistInfo}) => {
+			
+			const client = createDbClient(dbConfig);
 
+			await client.connect();
+			const { owner_id, github_id, owner_name, url } = gistInfo;
+
+			const query = {
+				text: 'INSERT INTO gists(owner_id, owner_name, github_id, url) VALUES($1, $2, $3, $4)',
+				values: [owner_id, owner_name, github_id, url],
+			};
+
+			await client.query(query);
+			await client.end();
+			// todo: if insert fails, throw error 
+			// todo: move this function to separate file
+			return { success: true, github_id };
+			
+		},
 };
--- githubApiWrapper.js
+++ githubApiWrapper.js
@@ -27,31 +27,4 @@ module.exports = {
 
 			return gistWithTransformedFiles;
 		},
-
-		createFavorite: async ({gistInfo}) => {
-			
-			const client = new Client({
-				user: 'postgres',
-				host: 'localhost',
-				database: 'gist_viewer',
-				password: 'password',
-				port: 5432,
-			});
-
-			await client.connect();
-			const { owner_id, github_id, owner_name, url } = gistInfo;
-
-			const query = {
-				text: 'INSERT INTO gists(owner_id, owner_name, github_id, url) VALUES($1, $2, $3, $4)',
-				values: [owner_id, owner_name, github_id, url],
-			};
-
-			await client.query(query);
-			await client.end();
-			// todo: if insert fails, throw error 
-			// todo: move this function to separate file
-			return { success: true, github_id };
-			
-		},
-
 };
